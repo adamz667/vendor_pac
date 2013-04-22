e539833b4d1d29be75f56b253ce594ec1a7e4be3
Jacek Adamski <adams@g.pl>
2013-04-22 19:20:51 +0200

    Add P760 overlay files and product
    Change-Id: I4cd4a4125e46f332ffebaf7c617a8ad8833d7cb0

---
+++ products/pac_p760.mk
@@ -0,0 +1,22 @@
+# Check for target product
+ifeq (pac_p760,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/device/p760
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# include ParanoidAndroid common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/lge/p760/cm.mk)
+
+PRODUCT_NAME := pac_p760
+
+endif
